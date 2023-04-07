2597fcd6e156aa710081ea4cfc1f808fa2c01aeb
juanlavallen <juanlavallen02@gmail.com>
2023-04-07 14:21:23 -0300

    add: email and email settings model

---
+++ src/Application/Models/EmailSettings.cs
@@ -0,0 +1,9 @@
+namespace Application.Models
+{
+    public class EmailSettings
+    {
+        public string? ApiKey { get; set; }
+        public string? FromAddress { get; set; }
+        public string? Name { get; set; }
+    }
+}
