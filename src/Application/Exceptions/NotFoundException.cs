476c0e63ff5bfc371182959d15034f205f78599d
juanlavallen <juanlavallen02@gmail.com>
2023-04-07 14:32:56 -0300

    add: bad request and not found exceptions

---
+++ src/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found")
+        {
+        }
+    }
+}
